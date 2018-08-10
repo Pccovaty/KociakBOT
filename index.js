b7a8ccb35c1ff104bab586e00319166dde147ee4
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-10 19:40:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
+bot.mutes = [];
 const coins = require("./coins.json");
 
 fs.readdir("./komendy/", (err, files) => {
