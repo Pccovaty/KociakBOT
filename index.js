508859ce7646c8aeae4d6868539dee25cc0cf6f0
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-10 20:14:43 +0200

    ulepszono witaj-zegnaj

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ bot.on("guildMemberAdd", async member => {
   const welcomechannel = member.guild.channels.find("name", "👋witaj-zegnaj👋");
   const welcomeEmebed = new Discord.RichEmbed()
   .setColor("#9b0090")
-  .addField(`Witaj ${member.user.username} na Gamers community`, `Na samym początku, zapoznaj się z regulaminem który jest na kanale <#435827081654304789> \n Jeżeli już przeczytałeś(aś) regulamin to możesz przejść do kanału <#461987816499445771>, gdzie możesz dodać sobie role typu 4FUN \n Mamy kanał do przedstawienia się, fajnie by było jakbyś wpadł(a) <#455059398033670144> \n Już wszystko zobaczyłeś(aś)? Przejdź na kanał <#435686053408538626> \n \n aktualnie jest nas: ${member.guild.memberCount}`);
+  .addField(`Witaj **${member.user.username}** na Our Community`, `**Na samym początku, zapoznaj się z regulaminem który jest na kanale <#435827081654304789> \n Jeżeli już przeczytałeś(aś) regulamin to możesz przejść do kanału <#461987816499445771>, gdzie możesz dodać sobie role typu 4FUN \n Mamy kanał do przedstawienia się, fajnie by było jakbyś wpadł(a) <#455059398033670144> \n Już wszystko zobaczyłeś(aś)? Przejdź na kanał <#435686053408538626> \n \n aktualnie jest nas: ${member.guild.memberCount}**`);
  welcomechannel.send(welcomeEmebed);
 });
 
