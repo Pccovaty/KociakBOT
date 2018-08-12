3e4fdde7f0919ff7bd594a79f8c7301e839679dd
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-12 14:13:53 +0200

    Update rzuc.js

--- komendy/rzuc.js
+++ komendy/rzuc.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-    let osoba = message.guild.member(message.mentions.usernames.first() || message.guild.members.get(args[0]));
-    message.channel.send(`${message.author.username} uderzył(a) poduszką ${osoba} \n https://nyanyan.it/upload/202203_85dph9ny12kgso64ztfixaqj0luwbe73vrcm.gif`);
+    let osoba = message.guild.member(message.mentions.users.usernames.first() || message.guild.members.get(args[0]));
+    message.channel.send(`**${message.author.username}** uderzył(a) poduszką ${osoba} \n https://nyanyan.it/upload/202203_85dph9ny12kgso64ztfixaqj0luwbe73vrcm.gif`);
     
 
 };
