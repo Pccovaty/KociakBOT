b186340c9ba3d668b3f2a131103489038dff86b9
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-13 15:05:52 +0200

    Update rzuc.js

--- komendy/rzuc.js
+++ komendy/rzuc.js
@@ -1,19 +1,16 @@
 const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-
-    let osoba = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    let rzucEmbed = new Discord.RichEmbed()
-    .setDescription("Wojna na poduszki, rozpoczęta")
-    .setColor("#bc0000")
-    .addField(`*${message.author.username}** uderzył(a) poduszką ${osoba}`)
-
-    message.channel.send(rzucEmbed)
     
+  let osoba = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  let embed = new Discord.RichEmbed()
+  .setDescription("Wojna na poduszki")
+  .setColor("#ffffff")
+  .addField(`**message.author.username** rzucił poduszką w osoba`)
+  .setImage("https://nyanyan.it/upload/202203_85dph9ny12kgso64ztfixaqj0luwbe73vrcm.gif")
+  message.channel.send(embed);
+}
 
-};
 module.exports.help = {
-  name: "rzuc"
-};
-
-
+  name:"rzuc"
+}
