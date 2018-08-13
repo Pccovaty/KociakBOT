03b7a19965e60c4227a64e87acdc7424c5b2a145
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-13 11:51:45 +0200

    Update rzuc.js

--- komendy/rzuc.js
+++ komendy/rzuc.js
@@ -3,7 +3,12 @@ const Discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
 
     let osoba = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    message.channel.send(`**${message.author.username}** uderzył(a) poduszką ${osoba} \n \n https://nyanyan.it/upload/202203_85dph9ny12kgso64ztfixaqj0luwbe73vrcm.gif`);
+    let banEmbed = new Discord.RichEmbed()
+    .setDescription("Wojna na poduszki, rozpoczęta")
+    .setColor("#bc0000")
+    .addField(`*${message.author.username}** uderzył(a) poduszką ${osoba}`)
+
+    message.channel.send(banEmbed)
     
 
 };
