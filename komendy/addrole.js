cb3bfd399d9ada745f6f9e17cb4b9affd6fb894a
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-13 11:42:23 +0200

    Update addrole.js

--- komendy/addrole.js
+++ komendy/addrole.js
@@ -6,24 +6,24 @@ module.exports.run = async (bot, message, args) => {
   //!addrole @andrew Dog Person
   if (!message.member.hasPermission("MANAGE_ROLES")) return errors.noPerms(message, "MANAGE_ROLES");
   if (args[0] == "help") {
-    message.reply("Usage: !addrole <user> <role>");
+    message.reply("Użycie: kb!addrole <mention> <Rola>");
     return;
   }
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if (!rMember) return errors.cantfindUser(message.channel);
   let role = args.join(" ").slice(22);
-  if (!role) return message.reply("Specify a role!");
+  if (!role) return message.reply("Podaj role!");
   let gRole = message.guild.roles.find(`name`, role);
-  if (!gRole) return message.reply("Couldn't find that role.");
+  if (!gRole) return message.reply("Nie znalazlem roli");
 
-  if (rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
+  if (rMember.roles.has(gRole.id)) return message.reply("Nie ma takiej roli...");
   await (rMember.addRole(gRole.id));
 
   try {
-    await rMember.send(`Congrats, you have been given the role ${gRole.name}`)
+    await rMember.send(`Brawo, <@${rMember.id}> otrzymałeś role **${gRole.name}**`)
   } catch (e) {
     console.log(e.stack);
-    message.channel.send(`Congrats to <@${rMember.id}>, they have been given the role ${gRole.name}. We tried to DM them, but their DMs are locked.`)
+    message.channel.send(`Brawo <@${rMember.id}>, właśnie dostałeś(aś) role ${gRole.name}.`)
   }
 }
 
