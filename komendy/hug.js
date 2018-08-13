44d1ce669b7e188316af25e09b7dd8792c100042
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-13 20:05:42 +0200

    poprawiono błąd

--- komendy/hug.js
+++ komendy/hug.js
@@ -5,7 +5,7 @@ const Discord = require("discord.js");
   let embed = new Discord.RichEmbed()
   .setAuthor(`${message.author.username}#${message.author.discriminator} przytulił(a) ${osoba.username}#${osoba.discriminator} ❤`)
    .setColor("#ffffff")
-  .setImage(https://media1.tenor.com/images/074d69c5afcc89f3f879ca473e003af2/tenor.gif?itemid=4898650")
+  .setImage("https://media1.tenor.com/images/074d69c5afcc89f3f879ca473e003af2/tenor.gif?itemid=4898650")
   message.channel.send(embed);
 }
  module.exports.help = {
