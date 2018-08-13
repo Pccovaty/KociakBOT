24cdc1ded8793c5c870e4ca62eade7a869cf3940
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-13 15:08:54 +0200

    Create rzuc.js

---
+++ komendy/rzuc.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+    
+  let osoba = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  let embed = new Discord.RichEmbed()
+  .setDescription("Wojna na poduszki")
+  .setColor("#ffffff")
+  .addField(`**${message.author.username}** rzucił(a) poduszką w ${osoba}`)
+  .setImage("https://nyanyan.it/upload/202203_85dph9ny12kgso64ztfixaqj0luwbe73vrcm.gif")
+  message.channel.send(embed);
+}
+
+module.exports.help = {
+  name:"rzuc"
+}
