d81410b069be992eb45001295cd8e057fbb44690
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-13 16:01:45 +0200

    Create rzuc.js

---
+++ komendy/rzuc.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+ module.exports.run = async(bot, message, args) => {
+    
+  let osoba = message.mentions.users.first();
+  let embed = new Discord.RichEmbed()
+  .setColor("#ffffff")
+  .setAuthor(`${message.author.username}#${message.author.discriminator} rzucił(a) poduszką w ${osoba.username}#${osoba.discriminator}`)
+  .setImage("https://nyanyan.it/upload/202203_85dph9ny12kgso64ztfixaqj0luwbe73vrcm.gif")
+  message.channel.send(embed);
+}
+ module.exports.help = {
+  name:"rzuc"
+}
