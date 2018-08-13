c8d97b0d806751d40c59547a58d5c4eede2dccdd
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-13 15:54:38 +0200

    aaaaaaaaaaaa

--- komendy/rzuc.js
+++ komendy/rzuc.js
@@ -5,7 +5,7 @@ module.exports.run = async(bot, message, args) => {
   let osoba = message.mentions.users.first();
   let embed = new Discord.RichEmbed()
   .setColor("#ffffff")
-  .setAuthor(`**${message.author.username}** rzucił(a) poduszką w **${osoba.username}**`)
+  .setAuthor(`${message.author.username}${message.author.tag} rzucił(a) poduszką w ${osoba.username}${osoba.tag}`)
   .setImage("https://nyanyan.it/upload/202203_85dph9ny12kgso64ztfixaqj0luwbe73vrcm.gif")
   message.channel.send(embed);
 }
