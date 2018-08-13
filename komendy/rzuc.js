5872e52702ef7016cedae574cedaa56bd556b6c1
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-13 20:00:13 +0200

    Update rzuc.js

--- komendy/rzuc.js
+++ komendy/rzuc.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js");
     
   let osoba = message.mentions.users.first();
   let embed = new Discord.RichEmbed()
-  .setColor("#ffffff")
   .setAuthor(`${message.author.username}#${message.author.discriminator} rzucił(a) poduszką w ${osoba.username}#${osoba.discriminator}`)
+   .setColor("#ffffff")
   .setImage("https://nyanyan.it/upload/202203_85dph9ny12kgso64ztfixaqj0luwbe73vrcm.gif")
   message.channel.send(embed);
 }
