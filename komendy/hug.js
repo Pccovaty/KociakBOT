6edf37ef55f4d912f1fdb97550de18516a7b17ee
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-13 20:04:03 +0200

    Utworzono hug.js

---
+++ komendy/hug.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+ module.exports.run = async(bot, message, args) => {
+    
+  let osoba = message.mentions.users.first();
+  let embed = new Discord.RichEmbed()
+  .setAuthor(`${message.author.username}#${message.author.discriminator} przytulił(a) ${osoba.username}#${osoba.discriminator} ❤`)
+   .setColor("#ffffff")
+  .setImage(https://media1.tenor.com/images/074d69c5afcc89f3f879ca473e003af2/tenor.gif?itemid=4898650")
+  message.channel.send(embed);
+}
+ module.exports.help = {
+  name:"hug"
+}
