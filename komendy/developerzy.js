8625b683e968b3a45fa858d21fa6625b500255f3
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-17 11:52:11 +0200

    Update developerzy.js

--- komendy/developerzy.js
+++ komendy/developerzy.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
   const bicon = bot.user.displayAvatarURL;
   const botembed = new Discord.RichEmbed()
-    .setDescription("Developerzy bota KociakBOT")
+    .setDescription("Developerzy bota ♕ Our Community ♕")
     .setColor("#9b0090")
     .setThumbnail(bicon)
     .addField("Global developerzy", "``๖̶̶̶ζ͜͡Kociak 💞#6365, ๖̶̶̶ۣۣۜۜ͜ζ͜͡Słodziak 💞#9231``")
