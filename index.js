f1b262dfad08bf3c9c994dcbbc53b7f40ea9c474
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-21 21:46:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,9 @@ const serverStats = {
   totalUsersID: "467226426563756032",
   memberCountID: "467276066717958156",
   botCountID: "467276143419064320",
-  onlinecountID: "481414408699117568"
+  onlinecountID: "481414408699117568",
+  onlineosobyID: "481549103193391104"
+  
 
 };
 bot.on("guildMemberAdd", member => {
@@ -71,7 +73,7 @@ bot.on("guildMemberAdd", member => {
   bot.channels.get(serverStats.memberCountID).setName(`✭ Ludzi: ${member.guild.members.filter(m => !m.user.bot).size}`);
   bot.channels.get(serverStats.onlinecountID).setName(`👋 ${member.user.username}`);
   bot.channels.get(serverStats.botCountID).setName(`✭ Botów: ${member.guild.members.filter(m => m.user.bot).size}`);
-
+  bot.channels.get(serverStats.onlineosobyID).setname(`✭ Online: ${member.user.presence.status}`);
 });
 bot.on("guildMemberRemove", member => {
 
@@ -80,6 +82,7 @@ bot.on("guildMemberRemove", member => {
   bot.channels.get(serverStats.totalUsersID).setName(`✭ Użytkowników: ${member.guild.memberCount}`);
   bot.channels.get(serverStats.memberCountID).setName(`✭ Ludzi: ${member.guild.members.filter(m => !m.user.bot).size}`);
   bot.channels.get(serverStats.botCountID).setName(`✭ Botów: ${member.guild.members.filter(m => m.user.bot).size}`);
+  bot.channels.get(serverStats.onlineosobyID).setname(`✭ Online: ${member.user.presence.status}`);
 });
 bot.on("guildMemberAdd", async member => {
   console.log(`${member.id} dołączył(a) na serwer.`);
