fd84ddcdd75adf487dab55031ffef53c4d7e3207
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-21 19:55:58 +0200

    Add files via upload

---
+++ NiesamowityŚwiatMyszkipl.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+
+  const dogembed = new Discord.RichEmbed()
+    .setTitle("Nowe partnerstwo!")
+    .addField("")
+    .setFooter("Partner: ");
+
+  message.channel.send(dogembed);
+};
+module.exports.help = {
+  name: "9"
+};
