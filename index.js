ea333e18c4dfceef61dbdf5464ae2808714c2b42
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-21 12:52:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -85,21 +85,25 @@ bot.on("ready", async() => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
   bot.user.setActivity("oc!pomoc | v3.2.2", {type: "STREAMING"});
-   glob.setItem('maxOnline', 0)
   
+});
+
+bot.("ready", async() => {
+
+glob.setItem('maxOnline', 0)
     if(count > parseInt(glob.getItem('maxOnline'))){
         glob.setItem('maxOnline', count)
         guild.channels.get('467276066717958156').setName('✭ Rekord Online: ' + count)
     }
     guild.channels.get('467276143419064320').setName('✭ Online: ' + count)
     guild.channels.get('481414408699117568').setName('✭ Administracja Online: ' + admCount)
-}
+
 
 bot.on('presenceUpdate', member => refreshOnline(member.guild))
 bot.on('guildMemberAdd', member => refreshCount(member.guild))
 bot.on('guildMemberRemove', member => refreshCount(member.guild))
   
-});
+})
 bot.on("message", async message => {
 
   if (message.author.bot) return;
