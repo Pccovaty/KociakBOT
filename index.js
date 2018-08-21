0a2281a505f34e4a326f1dd47ac46bda4ad14d08
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-21 21:58:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ bot.on("guildMemberAdd", member => {
   bot.channels.get(serverStats.memberCountID).setName(`✭ Ludzi: ${member.guild.members.filter(m => !m.user.bot).size}`);
   bot.channels.get(serverStats.onlinecountID).setName(`👋 ${member.user.username}`);
   bot.channels.get(serverStats.botCountID).setName(`✭ Botów: ${member.guild.members.filter(m => m.user.bot).size}`);
-  bot.channels.get(serverStats.onlineosobyID).setName(`✭ Online: ${member.user.presence.status.size}`);
+  bot.channels.get(serverStats.onlineosobyID).setName(`✭ Online: ${member.user.presence.status}`);
 });
 bot.on("guildMemberRemove", member => {
 
