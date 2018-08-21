5a2289faf94dc233030c7e1ad2c2b3559f1f0807
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-21 14:34:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ bot.on("guildMemberAdd", member => {
 
   bot.channels.get(serverStats.totalUsersID).setName(`✭ Użytkowników: ${member.guild.memberCount}`);
   bot.channels.get(serverStats.memberCountID).setName(`✭ Ludzi: ${member.guild.members.filter(m => !m.user.bot).size}`);
-  bot.channels.get(serverStats.onlinecountID).setName(`✭ 👋 ${member.user.username}`);
+  bot.channels.get(serverStats.onlinecountID).setName(`👋 ${member.user.username}`);
   bot.channels.get(serverStats.botCountID).setName(`✭ Botów: ${member.guild.members.filter(m => m.user.bot).size}`);
 
 });
@@ -65,7 +65,6 @@ bot.on("guildMemberRemove", member => {
   bot.channels.get(serverStats.totalUsersID).setName(`✭ Użytkowników: ${member.guild.memberCount}`);
   bot.channels.get(serverStats.memberCountID).setName(`✭ Ludzi: ${member.guild.members.filter(m => !m.user.bot).size}`);
   bot.channels.get(serverStats.botCountID).setName(`✭ Botów: ${member.guild.members.filter(m => m.user.bot).size}`);
-  bot.channels.get(serverStats.onlinecountID).setName(`✭ 👋 ${member.guild.members.filter(m => m.user.name)}`);
 });
 bot.on("guildMemberAdd", async member => {
   console.log(`${member.id} dołączył(a) na serwer.`);
