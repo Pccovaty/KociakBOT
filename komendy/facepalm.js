ac863408cbe5b83bc172784313654d78d088ae22
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-28 17:05:27 +0200

    Update facepalm.js

--- komendy/facepalm.js
+++ komendy/facepalm.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-   const facembed = new Discord.RichEmbed();
+   const facembed = new Discord.RichEmbed()
   .setDescription(`**${message.author.tag}**, twoja panda:`)
   .setColor('RANDOM')
   .setImage("https://media.giphy.com/media/3og0INyCmHlNylks9O/giphy.gif");
