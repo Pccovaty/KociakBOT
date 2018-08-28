6a69d61718b36363b793337f6b3a25aaad3f9990
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-28 12:38:59 +0200

    Update userinfo.js

--- komendy/userinfo.js
+++ komendy/userinfo.js
@@ -31,7 +31,7 @@ module.exports.run = async(bot, message, args) => {
     .addField("Łącznie napisanych wiadomości", messages[message.author.id].messages, true)
     .addField("Status", `${user.presence.status}`, true)
     .addField("W grze", `${user.presence.game ? user.presence.game.name : "Brak"}`, true)
-    .addField("Role", member.roles.map(roles => `${roles}`).join("👉"), true);
+    .addField("Role", member.roles.map(roles => `${roles}`).join("\👉"), true);
 
   message.channel.send(embed);
 
