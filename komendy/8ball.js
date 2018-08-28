2b6f7cdc7208d1d988d990901b4d75bfaa7ea3c5
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-08-28 14:16:15 +0200

    Update 8ball.js

--- komendy/8ball.js
+++ komendy/8ball.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
  const msg = await message.channel.send(":thinking:");
-  if(!args[1]) return message.reply("A czy pytanie nie powinno kończyć się znakiem zapytania? (?)");
+  if(!args[0]) return message.reply("A czy pytanie nie powinno kończyć się znakiem zapytania? (?)");
   let replies = ["Tak", "Nie", "Nie wiem", "Zapytaj później", "Chyba tak...", "Chyba nie...", "Być może", "Co to jest w ogóle za pytanie!?", "A jak myślisz?", "Moja odpowiedź brzmi nie"]
 
   let result = Math.floor((Math.random() * replies.length))
