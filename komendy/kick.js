a41b2f34d0af9382493ead7863ddded70b436c09
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-09-02 21:33:06 +0200

    Update kick.js

--- komendy/kick.js
+++ komendy/kick.js
@@ -2,37 +2,26 @@ const Discord = require("discord.js");
 const moment = require("moment");
 
 module.exports.run = async(bot, message, args) => {
-  const kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    const kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if (!kUser) return message.channel.send("Nie znaleziono użytkownika");
   const kReason = args.join(" ").slice(22);
   if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Nie możesz wyrzucić tej osoby!!");
   if (kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Nie możesz wyrzucić tej osoby!");
-  message.react("452183703267835910");
-  const kickEmbed = new Discord.RichEmbed()
-    .setDescription("~Kick~")
-    .setColor("#9b0090")
-    .addField("Wyrzucony Użytkownik", `${kUser}`)
-    .addField("ID wyrzuconego użytkownika", `${kUser.id}`)
-    .addField("Wyrzucony przez", `<@${message.author.id}>`)
-    .addField("Data i godzina", moment(message.createdAt).format("YYYY.MM.DD, HH:mm:ss"))
-    .addField("Powód", kReason);
-
-  const kickChannel = message.guild.channels.find("name", "kicki");
-  if (!kickChannel) return message.channel.send("Nie można znaleźć kanału ``kicki``");
-  message.channel.send(`Pomyślnie wyrzucono użytkownika: **${kUser}**, Powód: ${kReason}, więcej informacji na <#465570677135769600> `);
+  message.channel.send(`Pomyślnie wyrzucono użytkownika: **${kUser}**, Powód: ${kReason}`);
   message.guild.member(kUser).kick(kReason);
-  kickChannel.send(kickEmbed);
 
-  const embeeed = new Discord.RichEmbed()
-    .setDescription("O nie! Zostałeś wyrzucony z serwera **Our Community**!")
+  const kickeembed = new Discord.RichEmbed()
+    .setDescription("Kick")
     .setColor("#29ff00")
-    .addField("Zostałeś wyrzucony przez", `<@${message.author.id}>`)
+    .addField("Moderator", message.author.tag)
+    .addField("Wyrzucony", kUser)
     .addField("Powód", kReason)
-    .addField("Możesz wbić znowu link ponizej", "https://discord.gg/pppgZCV");
 
-  kUser.send(embeeed);
+    const incidentchannel = message.guild.channels.find("name", "mod-log");
+    if (!incidentchannel) return message.channel.send("Nie mogę znaleźć kanału. ``mod-log``");
 
-};
+    incidentchannel.send(kickeembed);
+
 
 module.exports.help = {
   name: "kick"
