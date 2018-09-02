f1de0bcfce4d7a00b0030f513318bcb5f3d86fc5
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-09-02 16:25:05 +0200

    Create pocaluj.js

---
+++ komendy/pocaluj.js
@@ -0,0 +1,21 @@
+  const Discord = require("discord.js");
+ module.exports.run = async(bot, message, args) => {
+ 
+    // jeśli osoba nikogo nie oznaczy, odpowiada ją wiadomością.
+      if (message.mentions.users.size < 1) return message.reply("**you need to mention someone**");
+    // jeśli osoba oznaczy siebie odpowiada jej wiadomością
+      if (message.mentions.users.first().id == message.author.id) return message.reply("**Are you... okay?...**")
+       // 'gifs.kiss" pochodzi z json, Math.floor i Math.random() wybiera randomowy link
+      var gifImage = gifs.kiss[Math.floor(Math.random()*gifs.kiss.length)];
+      const embed = new Discord.RichEmbed()
+          .setDescription(`@${message.author.tag} is kissing @${message.mentions.users.first().tag}`)
+          .setImage(gifImage)
+      message.channel.send({embed});
+ 
+ };
+ module.exports.help = {
+  name:"pocałuj"
+}
+  
+    
+ 
