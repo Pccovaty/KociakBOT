b01efe5b610fe00c53c2019609ccba52b1a06e0e
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-09-02 21:39:27 +0200

    Update ban.js

--- komendy/ban.js
+++ komendy/ban.js
@@ -2,36 +2,28 @@ const Discord = require("discord.js");
 const moment = require("moment");
 
 module.exports.run = async(bot, message, args) => {
-  const member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+ const member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if (!member) return message.channel.send("Najpierw oznacz użytkownika!");
   const bReason = args.join(" ").slice(22);
-  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":lock: Dostęp zablokowany! Nie posiadasz roli ``Moderator`` lub wyższej!`");
-  if (member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":lock: Dostęp zablokowany! Nie posiadasz roli ``Moderator`` lub wyższej!");
-    message.guild.member(member).ban(bReason);
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":lock: Dostęp zablokowany! Nie posiadasz roli ``moderator`` lub wyższej.");
+  if (member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":lock: Dostęp zablokowany! Nie posiadasz roli ``moderator`` lub wyższej.");
   const banEmbed = new Discord.RichEmbed()
-    .setDescription("~Ban~")
+    .setDescription("Ban")
     .setColor("#9b0090")
+    .addField("Moderator", message.author.tag)
     .addField("Zbanowany Użytkownik", `${member.user.tag}`)
-    .addField("Zbanowany przez", `${message.author.tag}`)
-    .addField("Zbanowany na kanale", message.channel)
     .addField("Powód", bReason);
 
   const incidentchannel = message.guild.channels.find("name", "mod-log");
   if (!incidentchannel) return message.channel.send("Nie mogę znaleźć kanału. ``mod-log``");
-  message.channel.send(`Pomyślnie zbanowano użytkownika: ${member.user.tag}, Powód: **${bReason}**.`);
-
-  const embeeed = new Discord.RichEmbed()
-    .setDescription("O nie! Zostałeś zbanowany na serwerze **♕ Our Community ♕**!")
-    .setColor("#ff0000")
-    .addField("Zostałeś zbanowany przez", `{message.author.tag}`)
-    .addField("Powód", bReason)
-    .addField("Unban all 1 Lipca 2019r", "https://discord.gg/pppgZCV");
-
-  bUser.send(embeeed);
+  message.channel.send(`Kolejny ban do kolekcji <:GWmythiBlobLUL:389447025592238092>`);
 
 
+
+  message.guild.member(member).ban(bReason);
   incidentchannel.send(banEmbed);
   message.react("452183703267835910");
+
 
 };
 
