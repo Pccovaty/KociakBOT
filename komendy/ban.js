d8e66fa0ba55bb07ce2f67d9cdf030cd67f1d6d4
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-09-02 21:20:40 +0200

    Update ban.js

--- komendy/ban.js
+++ komendy/ban.js
@@ -2,36 +2,33 @@ const Discord = require("discord.js");
 const moment = require("moment");
 
 module.exports.run = async(bot, message, args) => {
-  const bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if (!bUser) return message.channel.send("Oznacz użytkownika!");
+  const member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if (!member) return message.channel.send("Oznacz użytkownika!");
   const bReason = args.join(" ").slice(22);
-  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Brak uprawnień do komendy ``Ban``");
-  if (bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Brak uprawnień do komendy ``Ban``");
+  if (!message.member.hasPermission("Moderator")) return message.channel.send(":lock: Dostęp zablokowany! Nie posiadasz roli ``Moderator`` lub wyższej!`");
+  if (member.hasPermission("Moderator")) return message.channel.send(":lock: Dostęp zablokowany! Nie posiadasz roli ``Moderator`` lub wyższej!");
   const banEmbed = new Discord.RichEmbed()
     .setDescription("~Ban~")
     .setColor("#9b0090")
-    .addField("Zbanowany Użytkownik", `${bUser}`)
-    .addField("ID Zbanowanego", `${bUser.id}`)
-    .addField("Zbanowany przez", `<@${message.author.id}>`)
-    .addField("ID Osoby która zbanowała", `${message.author.id}`)
+    .addField("Zbanowany Użytkownik", `${member.user.tag}`)
+    .addField("Zbanowany przez", `${message.author.tag}`)
     .addField("Zbanowany na kanale", message.channel)
-    .addField("Data i godzina", moment(message.createdAt).format("DD.MM.YYYY, H:mm:ss"))
     .addField("Powód", bReason);
 
-  const incidentchannel = message.guild.channels.find("name", "bany");
-  if (!incidentchannel) return message.channel.send("Nie mogę znaleźć kanału. ``bany``");
-  message.channel.send(`Pomyślnie zbanowano użytkownika: ${bUser}, Powód: **${bReason}**, więcej informacji na <#465570658408202250>`);
+  const incidentchannel = message.guild.channels.find("name", "mod-log");
+  if (!incidentchannel) return message.channel.send("Nie mogę znaleźć kanału. ``mod-log``");
+  message.channel.send(`Pomyślnie zbanowano użytkownika: ${member.user.tag}, Powód: **${bReason}**.`);
 
   const embeeed = new Discord.RichEmbed()
     .setDescription("O nie! Zostałeś zbanowany na serwerze **♕ Our Community ♕**!")
     .setColor("#ff0000")
-    .addField("Zostałeś zbanowany przez", `<@${message.author.id}>`)
+    .addField("Zostałeś zbanowany przez", `{message.author.tag}`)
     .addField("Powód", bReason)
     .addField("Unban all 1 Lipca 2019r", "https://discord.gg/pppgZCV");
 
   bUser.send(embeeed);
 
-  message.guild.member(bUser).ban(bReason);
+  message.guild.member(member).ban(bReason);
   incidentchannel.send(banEmbed);
   message.react("452183703267835910");
 
