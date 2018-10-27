a433b4d99beb7be11e71db391d9fe21c7c728b08
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-10-27 09:10:17 +0200

    Update degrad.js

--- komendy/degrad.js
+++ komendy/degrad.js
@@ -1,18 +1,16 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js"); //importujesz discorda
 
-module.exports.run = async(bot, message, args) => {
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":lock: Dostęp zablokowany! Nie posiadasz roli z uprawnieniami ``MANAGE_MESSAGES``.");
-  if (member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":lock: Dostęp zablokowany! Nie posiadasz roli z uprawnieniami ``MANAGE_MESSAGES``.");
-    const target = message.mentions.users.first() || message.author;
-    const discord = new Discord.RichEmbed()
+module.exports.run = async(bot, message, args) => { //exportujesz komendę
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":lock: Dostęp zablokowany! Nie posiadasz roli z uprawnieniami ``MANAGE_MESSAGES``."); // jeżeli wiadomość.członekwysyłający.maUprawnienie("ZARZĄDZANIE WIADOMOŚCIAMI") === false, odpowiedz :lock: Dostęp zablokowany! Nie posiadasz roli z uprawnieniami ``MANAGE_MESSAGES``.
+    const target = message.mentions.users.first() || message.author; //bierzesz cel komendy
+    const discord = new Discord.RichEmbed() //nowy richembed i formatowanie go
     .setTitle(`Pomyślnie zdegradowano użytkownika: ${target.tag}`)
     .setColor("RANDOM")
     .setImage("https://vignette.wikia.nocookie.net/animal-jam-clans-1/images/b/b7/I%27m_not_a_psycho_for_liking_elsa_granhiert_stop.gif/revision/latest?cb=20170807063758")
     .setFooter(`Zdegradowany przez: ${message.author.tag}`, `${message.author.avatarURL}`)
     
-    message.channel.send(discord)
+    message.channel.send(discord) //wyslanie wiadomosci
 
-    message.guild.member(member)
     
 }
 
