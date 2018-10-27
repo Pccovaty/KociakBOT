bfb9fdad6a6c0735e905c24faf392e91c92bc26d
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-10-27 09:23:05 +0200

    Update degrad.js

--- komendy/degrad.js
+++ komendy/degrad.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js"); //importujesz discorda
 
 module.exports.run = async(bot, message, args) => { //exportujesz komendę
-    if (message.author.id !== "340557425511759892") return message.channel.send(":lock: | Dostęp zablokowany, Tą komendę może używać tylko osoba z rolą Zarząd lub Właściciel.");
-     if (message.author.id !== "333529845025865728") return message.channel.send(":lock: | Dostęp zablokowany, Tą komendę może używać tylko osoba z rolą Zarząd lub Właściciel.");
+   if (!(message.author.id === "340557425511759892" || message.author.id === "333529845025865728")) return message.channel.send(":lock: | Dostęp zablokowany, Tą komendę może używać tylko osoba z rolą Zarząd lub Właściciel.");
 //     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(":lock: Dostęp zablokowany! Nie posiadasz roli z uprawnieniami ``MANAGE_MESSAGES``."); // jeżeli wiadomość.członekwysyłający.maUprawnienie("ZARZĄDZANIE WIADOMOŚCIAMI") === false, odpowiedz :lock: Dostęp zablokowany! Nie posiadasz roli z uprawnieniami ``MANAGE_MESSAGES``.
     const target = message.mentions.users.first() || message.author; //bierzesz cel komendy
     const discord = new Discord.RichEmbed() //nowy richembed i formatowanie go
