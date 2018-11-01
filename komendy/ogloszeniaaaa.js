e350dfc9b7bbad8a8faff431febd2bb95beb2826
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-11-01 17:17:27 +0100

    Update ogloszeniaaaa.js

--- komendy/ogloszeniaaaa.js
+++ komendy/ogloszeniaaaa.js
@@ -2,20 +2,24 @@ const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-    const msg = await message.channel.send("<@&435826416488022017> Nowe ogloszenie!");
     if (!message.member.roles.find(r => r.name === "Administracja")) return message.channel.send("");
     if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("<:lock:485090231427661824> | Dostęp zablokowany! Nie posiadasz uprawnień ``Administrator``");
 
-    const embed = new Discord.RichEmbed()
-    .setTitle("Nowe ogloszenie!")
-    .setColor("RANDOM")
-    .addField("**Opis:**", args.join(" "))
-    .setFooter(`Ogłoszenie by: ${message.author.tag}`, `${message.author.avatarURL}`);
-
-    message.channel.send(embed)
-    msg.delete({timeout: 100000})
-    message.delete({timeout: 10000});
-};
-module.exports.help = {
-  name: "og"
-};
+          const tekst = args.join(' '); // czy tutaj xd
+          if (!tekst) return message.channel.send("Podaj tekst ogłoszenia");
+          let embed = new Discord.RichEmbed()
+            .setTitle("Nowe ogłoszenie")
+            .setColor("RANDOM")
+            .addField('Opis:', tekst)
+            .setFooter(`Ogłoszenie by: ${message.author.tag}`, `${message.author.avatarURL}`);
+          message.channel.send('<@&503980571828027409>' ).then(msg => msg.delete(500));
+          message.channel.send(embed);
+    
+    }
+    
+    
+    module.exports.help = {
+    
+      name:"og"
+    
+    }
