eb366aaa059076db891ccf336f4709497042907d
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-12-15 22:00:18 +0100

    Update reset.js

--- komendy/reset.js
+++ komendy/reset.js
@@ -1,4 +1,4 @@
-exports.run = async (client, message, args) => {
+exports.run = async (bot, message, args) => {
     if (message.author.id === '340557425511759892') {
         if (!args || args.size < 1) return message.channel.send("Musisz podać komendę do przeładowania...");
         delete require.cache[require.resolve(`./${args[0]}.js`)];
