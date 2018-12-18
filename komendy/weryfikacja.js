1730e5e36821b6aa76581a491df209c278e933c1
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-12-18 15:18:13 +0100

    Update weryfikacja.js

--- komendy/weryfikacja.js
+++ komendy/weryfikacja.js
@@ -3,8 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     message.delete();
     let role = message.guild.roles.find(role => role.name === 'Uzytkownicy');
+    let rolee = message.guild.roles.find(role => role.name === 'Nowy(a)');
     if (message.channel.name !== 'weryfikacja') return message.reply('Musisz dołączyć na kanał <#523602318168686602>');
     message.member.addRole(role);
+    message.member.removeRole(rolee);
     if (message.member.roles.has(role.id)) {
         let verifyEmbed = new Discord.RichEmbed()
             .setAuthor(message.member.displayName, message.author.displayAvatarURL)
