5657b3e9431711fa4a6c47b1b4d3afbb878ec896
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-12-23 19:35:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,21 @@ const fs = require("fs");
 const moment = require('moment');
 const bot = new Discord.Client({disableEveryone: true});
 moment.locale('PL');
-const antispam = require("discord-anti-spam"); //the main function for the anti spam
 bot.commands = new Discord.Collection();
 bot.mutes = [];
+const antispam = require("discord-anti-spam");
 
 antispam(bot, {
-    warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned.
-    maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
-    interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
-    warningMessage: "Cześć, Nie próbuj spamić bo dostaniesz bana...", // Warning message send to the user indicating they are going to fast.
-    banMessage: "Dostał bana, Nie radzimy spamić ^^", // Ban message, always tags the banned user in front of it.
-    maxDuplicatesWarning: 7, // Maximum amount of duplicate messages a user can send in a timespan before getting warned
-    maxDuplicatesBan: 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned
-    deleteMessagesAfterBanForPastDays: 7 // Delete the spammed messages after banning for the past x days.
+  warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned.
+  maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
+  interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
+  warningMessage: "Przestaniesz spamić? [3/5]", // Warning message send to the user indicating they are going to fast.
+  banMessage: "I kolega dostał bana. Ktoś jeszcze?", // Ban message, always tags the banned user in front of it.
+  maxDuplicatesWarning: 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned
+  maxDuplicatesBan: 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned
+  deleteMessagesAfterBanForPastDays: 7 // Delete the spammed messages after banning for the past x days.
+  exemptRoles: ["Administracja", "Chat Moderator"] // The names of the roles which should not be spam-filtered
+  exemptUsers: ["Kociak#0001"] // The Discord tags of the users who should not be spam-filtered
 });
 
 var coins = require("./coins.json");
