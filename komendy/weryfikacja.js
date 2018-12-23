6e455b64ee00b4f8c74b15576556143f9bc8d16a
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2018-12-23 22:06:45 +0100

    Update weryfikacja.js

--- komendy/weryfikacja.js
+++ komendy/weryfikacja.js
@@ -1,23 +1,25 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (bot, message, args, msg) => {
     message.delete();
     let role = message.guild.roles.find(role => role.name === 'Uzytkownicy');
+    let rolee = message.guild.roles.find(role => role.name === 'Nowy(a)');
     if (message.channel.name !== 'weryfikacja') return message.reply('Musisz dołączyć na kanał <#523602318168686602>');
     message.member.addRole(role);
+    message.member.removeRole(rolee)
     if (message.member.roles.has(role.id)) {
         let verifyEmbed = new Discord.RichEmbed()
             .setAuthor(message.member.displayName, message.author.displayAvatarURL)
             .setColor('#36393f')
             .setDescription('Twoje konto zostało już zweryfikowane!')
-        return message.channel.send((verifyEmbed));
+        return msg.channel.send((verifyEmbed));
     } else {
         let verifyEmbed = new Discord.RichEmbed()
             .setAuthor(message.member.displayName, message.author.displayAvatarURL)
             .setColor('#36393f')
             .setDescription('Twoje konto zostało pomyślnie zweryfikowane.')
-        return message.channel.send((verifyEmbed));
-         
+        return msg.channel.send((verifyEmbed));
+         msg.delete({timeout: 10000})
     }
 }
 
