9169ad468a98acc10faea586e46877d16d3cb73b
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2019-01-10 18:54:04 +0100

    Update weryfikacja.js

--- komendy/weryfikacja.js
+++ komendy/weryfikacja.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args, msg) => {
     message.delete();
     let role = message.guild.roles.find(role => role.name === 'Uzytkownicy');
     let rolee = message.guild.roles.find(role => role.name === 'Nowy(a)');
-    if (message.channel.name !== '523602318168686602') return message.reply('Musisz dołączyć na kanał <#523602318168686602>');
+    if (message.channel.name !== 'weryfikacja') return message.reply('Musisz dołączyć na kanał <#523602318168686602>');
     message.member.addRole(role);
     message.member.removeRole(rolee)
     if (message.member.roles.has(role.id)) {
