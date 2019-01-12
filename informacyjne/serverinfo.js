e5f1c8618e0c5900de92b723adefb476cb301e68
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2019-01-12 22:21:21 +0100

    Update serverinfo.js

--- informacyjne/serverinfo.js
+++ informacyjne/serverinfo.js
@@ -8,18 +8,11 @@ module.exports.run = async(bot, message, args) => {
  
  
   const serverembed = new Discord.RichEmbed()
-    .setTitle(message.guild.name, sicon)
-    .setColor(role.hexcolor)
+    .setTitle(`Informacje dla serwera ${message.guild.name}`)
+    .setColor("RANDOM")
     .setThumbnail(sicon)
-    .addField("Właściciel", message.guild.owner, true)
-    .addField("Nazwa serwera", message.guild.name, true)
-    .addField("Region", message.guild.region, true)
-    .addField("Kanał AFK", message.guild.afkChannel || "Brak", true)
+    .setDescription(`**Właściciel:** ${message.guild.owner} \n **ID serwera:** ${message.guild.id} \n  **Stworzony:** ${moment(message.guild.createdAt).format("DD.MM.YYYY, H:mm:ss")} \n **liczba członków:** ${message.guild.memberCount}`)
     .setTimestamp()
-    .addField('Status Członków', `**${message.guild.members.filter(o => o.presence.status === 'online').size}** Online\n**${message.guild.members.filter(i => i.presence.status === 'idle').size}** zaraz wracam\n**${message.guild.members.filter(dnd => dnd.presence.status === 'dnd').size}** Nie przeszkadzać\n**${message.guild.members.filter(off => off.presence.status === 'offline').size}** Offline/Niewidoczny\n**${message.guild.members.filter(s => s.presence.status === 'streaming').size}** Prowadzi Stream`)
-    .addField("Serwer stworzony dnia:", moment(message.guild.createdAt).format("DD.MM.YYYY, H:mm:ss", true))
-    .addField("Dołączyłeś na serwer dnia:", moment(message.member.joinedAt).format("DD.MM.YYYY, H:mm:ss", true))
-    .addField("Łączna liczba członków", message.guild.memberCount, true);
 
 
     
