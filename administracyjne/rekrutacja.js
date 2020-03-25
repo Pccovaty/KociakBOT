3d50d7e16e894ff6abaa5839af40fda355ccf640
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2020-03-25 08:41:28 +0100

    Update rekrutacja.js

--- administracyjne/rekrutacja.js
+++ administracyjne/rekrutacja.js
@@ -2,30 +2,24 @@ const Discord = require("discord.js");
 const agree = "526698392810815518"
 const nieagre = "526698394543194112"
 module.exports.run = async(bot, message, args, msg) => {
-  const incidentchannel = message.guild.channels.find('id', '531565730731130900');//mod-log channel name. change for you
-  if (!incidentchannel) return message.channel.send("Nie mogę znaleźć kanału. ``ID: 531565730731130900``");
+  const incidentchannel = message.guild.channels.find('id', '692268510075813901');//mod-log channel name. change for you
+  if (!incidentchannel) return message.channel.send("Nie mogę znaleźć kanału. ``ID: 692268510075813901``");
     cmd = message.content;
     const heelp = new Discord.RichEmbed()
-    .setTitle("Wzór na rangę Moderator")
-    .setDescription("~~**Twój nick na Discordzie wraz z Hasztagiem (nick#0000)\n Co Twoim zdaniem należy do obowiązków Junior Moderatora? \n Ile masz lat? \n Jak masz na imie? \n Dlaczego ty masz zostać Junior Moderatorem? \n Jak często przebywasz na Discordzie? \n Dlaczego mamy Cię wybrać? \n Min 3 zdania o sobie**\n \n  ~~ \n \n \n Rekrutacja: OFF")
-    .addField("Jak napisać poprawnie rekrutacje?", "~~**Aby poprawnie napisać rekrutacje, wystarczy ze napiszesz je tak jak ponizszy przyklad** \n \n \n Użyj: **!!rekrutacja 1;2;3;4;5;6;7;8** < - zamiast liczb 1;2;3; itp daj swoje odpowiedzi~~")
+    .setTitle("Wzór na weryfikacje")
+    .setDescription("**Jesteś Nauczycielem czy Uczniem? (jeśli uczniem, to Elektryk czy Mechatronik)\n Twoje Imie i Nazwisko? \n (Krótka wiedza) Po co stworzyliśmy ten serwer?**")
+    .addField("Jak napisać poprawnie weryfikacje?", "**Aby poprawnie napisać weryfikacje, wystarczy ze napiszesz je tak jak ponizszy przyklad** \n \n \n Użyj: **!!w 1;2;3;4;5;6;7;8** < - zamiast liczb 1;2;3; itp daj swoje odpowiedzi")
     .setColor("BLUE")
-    .setFooter("")
+    .setFooter("1ck | Weryfikacja stworzona przez Mateusz Bieniek")
     params = cmd.substr(cmd.split(" ", 1)[0].length + 1, cmd.length).split(";");
   if (params.length !== 8) return message.channel.send(heelp);
     
          const embed = new Discord.RichEmbed()
-        .addField("Twój nick na Discordzie wraz z Hasztagiem (nick#0000)", params[0])
-        .addField("Co Twoim zdaniem należy do obowiązków Junior Moderatora?", params[1])
-        .addField("Ile masz lat?", params[2])
-        .addField("Jak masz na imie?", params[3])
-        .addField("Dlaczego ty masz zostać Junior Moderatorem?", params[4])
-        .addField("Jak często przebywasz na Discordzie?", params[5])
-        .addField("Dlaczego mamy Cię wybrać?", params[6])
-        .addField("Min 3 zdania o sobie", params[7])
+        .addField("Jesteś Nauczycielem czy Uczniem? (jeśli uczniem, to Elektryk czy Mechatronik)", params[0])
+        .addField("Twoje Imie i Nazwisko?", params[1])
+        .addField("(Krótka wiedza) Po co stworzyliśmy ten serwer?", params[2])
         .addField("ID", message.author.id)
         .addField("Nazwa", message.author.tag)
-        
         .setColor("RED")
     if (args[0] == "pomoc"){
      message.channel.send(help)
@@ -41,5 +35,5 @@ module.exports.run = async(bot, message, args, msg) => {
         };
 
 module.exports.help = {
-  name: "wesdxrcfctvgyazwsexdrcftvgwsexdrcftvgybws3edxrcftvgbyhwsedrxcfvtgbyhed4rftvgybhwsexdrcfvtgbws3ed4rcftvgby"
+  name: "w"
 };
