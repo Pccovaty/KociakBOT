ce91eaea6c9580827ff89863851362a1e5c366b3
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2020-03-25 09:02:48 +0100

    Update rekrutacja.js

--- administracyjne/rekrutacja.js
+++ administracyjne/rekrutacja.js
@@ -1,6 +1,4 @@
 const Discord = require("discord.js");
-const agree = "526698392810815518"
-const nieagre = "526698394543194112"
 module.exports.run = async(bot, message, args, msg) => {
   const incidentchannel = message.guild.channels.find('id', '692279961767379005');//mod-log channel name. change for you
   if (!incidentchannel) return message.channel.send("Nie mogę znaleźć kanału. ``ID: 692279961767379005``");
@@ -8,7 +6,7 @@ module.exports.run = async(bot, message, args, msg) => {
     const heelp = new Discord.RichEmbed()
     .setTitle("Wzór na weryfikacje")
     .setDescription("**Jesteś Nauczycielem czy Uczniem? (jeśli uczniem, to Elektryk czy Mechatronik)\n Twoje Imie i Nazwisko? \n (Krótka wiedza) Po co stworzyliśmy ten serwer?**")
-    .addField("Jak napisać poprawnie weryfikacje?", "**Aby poprawnie napisać weryfikacje, wystarczy ze napiszesz je tak jak ponizszy przyklad** \n \n \n Użyj: **!!w 1;2;3;4;5;6;7;8** < - zamiast liczb 1;2;3; itp daj swoje odpowiedzi")
+    .addField("Jak napisać poprawnie weryfikacje?", "**Aby poprawnie napisać weryfikacje, wystarczy ze napiszesz je tak jak ponizszy przyklad** \n \n \n Użyj: **!!w 1;2;3** < - zamiast liczb 1;2;3; itp daj swoje odpowiedzi")
     .setColor("BLUE")
     .setFooter("1ck | Weryfikacja stworzona przez Mateusz Bieniek")
     params = cmd.substr(cmd.split(" ", 1)[0].length + 1, cmd.length).split(";");
@@ -25,9 +23,6 @@ module.exports.run = async(bot, message, args, msg) => {
      message.channel.send(heelp)
     } else  {
         const msg = await incidentchannel.send(embed);
-
-        await msg.react("526698392810815518");
-        await msg.react("526698394543194112");
       
         message.delete({timeout: 1000});
             }
