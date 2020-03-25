815bd39756549c4559071d2abd1eaa1469d6e58b
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2020-03-25 09:01:36 +0100

    Update rekrutacja.js

--- administracyjne/rekrutacja.js
+++ administracyjne/rekrutacja.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 const agree = "526698392810815518"
 const nieagre = "526698394543194112"
 module.exports.run = async(bot, message, args, msg) => {
-  const incidentchannel = message.guild.channels.find('id', '692268510075813901');//mod-log channel name. change for you
-  if (!incidentchannel) return message.channel.send("Nie mogę znaleźć kanału. ``ID: 692268510075813901``");
+  const incidentchannel = message.guild.channels.find('id', '692279961767379005');//mod-log channel name. change for you
+  if (!incidentchannel) return message.channel.send("Nie mogę znaleźć kanału. ``ID: 692279961767379005``");
     cmd = message.content;
     const heelp = new Discord.RichEmbed()
     .setTitle("Wzór na weryfikacje")
@@ -12,7 +12,7 @@ module.exports.run = async(bot, message, args, msg) => {
     .setColor("BLUE")
     .setFooter("1ck | Weryfikacja stworzona przez Mateusz Bieniek")
     params = cmd.substr(cmd.split(" ", 1)[0].length + 1, cmd.length).split(";");
-  if (params.length !== 8) return message.channel.send(heelp);
+  if (params.length !== 3) return message.channel.send(heelp);
     
          const embed = new Discord.RichEmbed()
         .addField("Jesteś Nauczycielem czy Uczniem? (jeśli uczniem, to Elektryk czy Mechatronik)", params[0])
