6227156706d6abf664b162c06020e01f891981df
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2021-04-01 13:00:05 +0200

    Update sayembed.js

--- administracyjne/sayembed.js
+++ administracyjne/sayembed.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-  if (!message.member.roles.find(r => r.name === "Administracja")) return message.channel.send("❌ Błąd | Nie posiadasz roli  ``Administracja``");
   const sayMessage = args.join(" ");
   message.delete().catch();
   const embed = new Discord.RichEmbed()
