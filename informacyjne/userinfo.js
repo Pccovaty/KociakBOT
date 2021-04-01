87a4e2a7b103550d68accc49193de421d7548942
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2021-04-01 12:25:38 +0200

    Update userinfo.js

--- informacyjne/userinfo.js
+++ informacyjne/userinfo.js
@@ -1,7 +1,6 @@
-
 const Discord = require("discord.js");
 const moment = require("moment");
-const messages = require("../messages.json");
+
 
 module.exports.run = async(bot, message, args) => {
   let user;
@@ -11,12 +10,7 @@ module.exports.run = async(bot, message, args) => {
   } else {
     user = message.author;
   }
-  //Messagese
-  if (!messages[message.author.id]) {
-    messages[message.author.id] = {
-      messages: 0
-    };
-  }
+
 
   const member = message.guild.member(user);
 
@@ -24,7 +18,7 @@ module.exports.run = async(bot, message, args) => {
     .setColor("RANDOM")
     .setThumbnail(user.avatarURL)
     .setTitle(`${user.username}#${user.discriminator}`)
-    .setDescription(`**Pseudonim:** ${member.nickname !== null ? `${member.nickname}` : "Brak pseudonimu"} \n **Dołączył na Discord:** ${moment(user.createdAt).format('DD.MM.YYYY HH:mm:ss')} \n **Dołączł na serwer:** ${moment(member.joinedAt).format("DD.MM.YYYY HH:mm:ss")} \n **Łącznie napisanych wiadomości:** ${messages[message.author.id].messages} \n **Status:** ${user.presence.status} \n **w grze:** ${user.presence.game ? user.presence.game.name : "Użytkownik nie gra w żadną gre"} \n **Role:** ${member.roles.size} więcej informacji pod komendą **__!!profil role__** `)
+    .setDescription(`**Pseudonim:** ${member.nickname !== null ? `${member.nickname}` : "Brak pseudonimu"} \n **Dołączył na Discord:** ${moment(user.createdAt).format('DD.MM.YYYY HH:mm:ss')} \n **Dołączł na serwer:** ${moment(member.joinedAt).format("DD.MM.YYYY HH:mm:ss")}  \n **Status:** ${user.presence.status} \n **w grze:** ${user.presence.game ? user.presence.game.name : "Użytkownik nie gra w żadną gre"} \n **Role:** ${member.roles.size} więcej informacji pod komendą **__!!profil role__** `)
 
     const profilkolor = new Discord.RichEmbed()
     .setThumbnail(user.avatarURL)
