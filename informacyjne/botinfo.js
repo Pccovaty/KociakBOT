09333b3faa716008be40f9ddbdcda976b5ffdc8b
Pccovaty <31142496+Pccovaty@users.noreply.github.com>
2021-04-01 12:18:20 +0200

    Update botinfo.js

--- informacyjne/botinfo.js
+++ informacyjne/botinfo.js
@@ -8,7 +8,7 @@ module.exports.run = async(bot, message, args) => {
     .setColor("RANDOM")
     .setThumbnail(bicon)
     .setFooter(`${moment().calendar()}  |  ${message.author.tag}`, `${message.author.avatarURL}`)
-    .setDescription(`**Nazwa bota:** ${bot.user.tag} \n **Autor bota:** <@340557425511759892> \n **Pomoc w bocie:** <@396284197389729793>, <@151436376712151041>, <@267761613438713876>, <@249602562372665346> \n **Język bota:** [discord.js](https://discord.js.org/)`)
+    .setDescription(`**Nazwa bota:** ${bot.user.tag} \n **Autor bota:** <@340557425511759892> \n **Pomoc w bocie:** <@396284197389729793>, <@151436376712151041>, <@267761613438713876>, \n **Język bota:** [discord.js](https://discord.js.org/)`)
   message.channel.send(botembed);
   message.react("452183703267835910");
 };
